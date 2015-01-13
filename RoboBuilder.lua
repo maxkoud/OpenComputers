35a714e972ba661af41b88f5e2a006ee687080ef
ne0lithic <ne0lithic@users.noreply.github.com>
2015-01-13 22:58:54 +0300

    place()
    Проверка инвентаря

--- RoboBuilder.lua
+++ RoboBuilder.lua
@@ -1,1 +1,14 @@
-local robot = require("robot")
+local r = require("robot")
+
+r.select(1)
+
+function place()
+  if r.count>0 then
+    r.placeDown()
+  else
+    for inv=1,16 do
+      r.select(inv)
+      if r.count>0 then brake() end
+    end
+  end
+end
